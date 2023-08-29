5caa3568b2ce7f72a7e683512c3112a6d89eb025
Daniel Flores <dflores@i4digital.com>
2023-08-29 15:45:27 -0500

    ajuste

--- app/Middleware/Auth.ts
+++ app/Middleware/Auth.ts
@@ -3,41 +3,68 @@ import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import Env from "@ioc:Adonis/Core/Env";
 import { EResponseCodes } from "App/Constants/ResponseCodesEnum";
 import { ApiResponse } from "App/Utils/ApiResponses";
-
-
-export interface IDecodedToken {
-  id: number;
-}
+import Encryption from "@ioc:Adonis/Core/Encryption";
 
 export default class Auth {
   public async handle(
-    { request, response }: HttpContextContract,
-    next: () => Promise<void>
+    ctx: HttpContextContract,
+    next: () => Promise<void>,
+    guards: string[]
   ) {
-    try {
-      const { authorization: bearerToken } = request.headers();
+    const { authorization, permissions } = ctx.request.headers();
+    const key = Env.get("APP_KEY");
 
-      if (!bearerToken) {
-        return response.forbidden(
-          new ApiResponse(null, EResponseCodes.FAIL, "No existe el token")
-        );
+    try {
+      if (!authorization) {
+        return ctx.response
+          .status(401)
+          .send(
+            new ApiResponse(
+              null,
+              EResponseCodes.FAIL,
+              "El token no ha sido proporcionado!."
+            )
+          );
       }
 
-      const accessToken = bearerToken.split(" ")[1];
+      const { id, document } = jwt.verify(
+        authorization.replace("Bearer ", ""),
+        key
+      ) as { id: number; document: string };
 
-      const { id } = jwt.verify(
-        accessToken,
-        Env.get("APP_KEY")
-      ) as IDecodedToken;
+      if (!id) {
+        return ctx.response
+          .status(401)
+          .send(new ApiResponse(null, EResponseCodes.FAIL, "Token no valido"));
+      }
+      Env.set("CURRENT_AUTHORIZATION", authorization || "none");
+      Env.set("CURRENT_PERMISSIONS", permissions || "none");
+      Env.set("CURRENT_USER_DOCUMENT", document || "none");
 
-      request["idUser"] = id;
+      if (guards?.length > 0) {
+        const subkey = authorization.split(".")[2];
+        const accessEncryptor = Encryption.child({
+          secret: subkey + Env.get("APP_KEY"),
+        });
 
-      await next();
+        const access: Array<string> | null = JSON.parse(
+          accessEncryptor.decrypt(String(permissions)) || ""
+        );
+
+        if (!access?.find((i) => i === guards.find((g) => g === i))) {
+          return ctx.response
+            .status(403)
+            .send(
+              new ApiResponse(null, EResponseCodes.FAIL, "Acceso no autorizado")
+            );
+        }
+      }
     } catch (error) {
-      return response.forbidden(
-        new ApiResponse(null, EResponseCodes.FAIL, String(error))
-      );
+      return ctx.response
+        .status(401)
+        .send(new ApiResponse(null, EResponseCodes.FAIL, "Token no valido!"));
     }
+
+    await next();
   }
 }
-
