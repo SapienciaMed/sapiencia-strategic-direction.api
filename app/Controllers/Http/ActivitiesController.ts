a076733084b41c5c53703998a9e043a4938fdd82
Santiago Ochoa <sochoa@i4digital.com>
2023-09-25 00:27:53 -0500

    Bug consolidado actividades

--- app/Controllers/Http/ActivitiesController.ts
+++ app/Controllers/Http/ActivitiesController.ts
@@ -16,64 +16,82 @@ export default class ComponentsController {
             activitiesData.forEach((item, index) => {
                 const stage = stages.data.find(stage => stage.id === item.stageActivity)
                 const gruped = index > 0 ? JSON.stringify(activitiesData[index - 1].objetiveActivity) === JSON.stringify(item.objetiveActivity) : false;
-                item.detailActivities.forEach((detail, index) => {
-                    const component = components.data.find(component => component.id === detail.component)
-                    if (index === 0) {
-                        content.push({
-                            objetiveActivity: {
-                                consecutive: gruped ? null : item.objetiveActivity.consecutive,
-                                description: gruped ? null : item.objetiveActivity.description
-                            },
-                            productMGA: item.productMGA,
-                            productDescriptionMGA: item.productDescriptionMGA,
-                            activityMGA: item.activityMGA,
-                            activityDescriptionMGA: item.activityDescriptionMGA,
-                            stageActivity: stage ? stage.description : item.stageActivity,
-                            budgetsMGA: item.budgetsMGA,
-                            validity: item.validity,
-                            year: item.year,
-                            detailActivity: {
-                                consecutive: detail.consecutive,
-                                detailActivity: detail.detailActivity,
-                                component: component ? component.description : detail.component,
-                                measurement: detail.measurement,
-                                amount: detail.amount,
-                                unitCost: detail.unitCost,
-                                pospre: detail.pospre,
-                                validatorCPC: detail.validatorCPC,
-                                clasificatorCPC: detail.clasificatorCPC,
-                                sectionValidatorCPC: detail.sectionValidatorCPC,
-                            }
-                        })
-                    } else {
-                        content.push({
-                            objetiveActivity: {
-                                consetutive: null,
-                                description: null
-                            },
-                            productMGA: null,
-                            productDescriptionMGA: null,
-                            activityMGA: null,
-                            activityDescriptionMGA: null,
-                            stageActivity: "",
-                            budgetsMGA: {},
-                            validity: "",
-                            year: "",
-                            detailActivity: {
-                                consecutive: detail.consecutive,
-                                detailActivity: detail.detailActivity,
-                                component: component ? component.description : detail.component,
-                                measurement: detail.measurement,
-                                amount: detail.amount,
-                                unitCost: detail.unitCost,
-                                pospre: detail.pospre,
-                                validatorCPC: detail.validatorCPC,
-                                clasificatorCPC: detail.clasificatorCPC,
-                                sectionValidatorCPC: detail.sectionValidatorCPC,
-                            }
-                        })
-                    }
-                })
+                if(item.detailActivities.length > 0) {
+                    item.detailActivities.forEach((detail, index) => {
+                        const component = components.data.find(component => component.id === detail.component)
+                        if (index === 0) {
+                            content.push({
+                                objetiveActivity: {
+                                    consecutive: gruped ? null : item.objetiveActivity.consecutive,
+                                    description: gruped ? null : item.objetiveActivity.description
+                                },
+                                productMGA: item.productMGA,
+                                productDescriptionMGA: item.productDescriptionMGA,
+                                activityMGA: item.activityMGA,
+                                activityDescriptionMGA: item.activityDescriptionMGA,
+                                stageActivity: stage ? stage.description : item.stageActivity,
+                                budgetsMGA: item.budgetsMGA,
+                                validity: item.validity,
+                                year: item.year,
+                                detailActivity: {
+                                    consecutive: detail.consecutive,
+                                    detailActivity: detail.detailActivity,
+                                    component: component ? component.description : detail.component,
+                                    measurement: detail.measurement,
+                                    amount: detail.amount,
+                                    unitCost: detail.unitCost,
+                                    pospre: detail.pospre,
+                                    validatorCPC: detail.validatorCPC,
+                                    clasificatorCPC: detail.clasificatorCPC,
+                                    sectionValidatorCPC: detail.sectionValidatorCPC,
+                                }
+                            });
+                        } else {
+                            content.push({
+                                objetiveActivity: {
+                                    consetutive: null,
+                                    description: null
+                                },
+                                productMGA: null,
+                                productDescriptionMGA: null,
+                                activityMGA: null,
+                                activityDescriptionMGA: null,
+                                stageActivity: "",
+                                budgetsMGA: {},
+                                validity: "",
+                                year: "",
+                                detailActivity: {
+                                    consecutive: detail.consecutive,
+                                    detailActivity: detail.detailActivity,
+                                    component: component ? component.description : detail.component,
+                                    measurement: detail.measurement,
+                                    amount: detail.amount,
+                                    unitCost: detail.unitCost,
+                                    pospre: detail.pospre,
+                                    validatorCPC: detail.validatorCPC,
+                                    clasificatorCPC: detail.clasificatorCPC,
+                                    sectionValidatorCPC: detail.sectionValidatorCPC,
+                                }
+                            });
+                        }
+                    });
+                } else {
+                    content.push({
+                        objetiveActivity: {
+                            consecutive: gruped ? null : item.objetiveActivity.consecutive,
+                            description: gruped ? null : item.objetiveActivity.description
+                        },
+                        productMGA: item.productMGA,
+                        productDescriptionMGA: item.productDescriptionMGA,
+                        activityMGA: item.activityMGA,
+                        activityDescriptionMGA: item.activityDescriptionMGA,
+                        stageActivity: stage ? stage.description : item.stageActivity,
+                        budgetsMGA: item.budgetsMGA,
+                        validity: item.validity,
+                        year: item.year,
+                        detailActivity: null
+                    });
+                }
             })
             let data = [
                 {
@@ -83,24 +101,24 @@ export default class ComponentsController {
                         { label: "Producto MGA", value: (row) => row.productMGA ? `${row.productMGA}. ${row.productDescriptionMGA}` : "" },
                         { label: "Actividad MGA", value: (row) => row.productMGA ? `${row.activityMGA}. ${row.activityDescriptionMGA}` : "" },
                         { label: "Etapa", value: (row) => row.stageActivity },
-                        { label: "Año 0", value: (row) => row.budgetsMGA.year0?.budget, format: "$0.00" },
-                        { label: "Año 1", value: (row) => row.budgetsMGA.year1?.budget, format: "$0.00" },
-                        { label: "Año 2", value: (row) => row.budgetsMGA.year2?.budget, format: "$0.00" },
-                        { label: "Año 3", value: (row) => row.budgetsMGA.year3?.budget, format: "$0.00" },
-                        { label: "Año 4", value: (row) => row.budgetsMGA.year4?.budget, format: "$0.00" },
-                        { label: "Presupuesto", value: (row) => row.productMGA ? row.budgetsMGA.year0?.budget + row.budgetsMGA.year1?.budget + row.budgetsMGA.year2?.budget + row.budgetsMGA.year3?.budget + row.budgetsMGA.year4?.budget : null, format: "$0.00" },
+                        { label: "Año 0", value: (row) => row.budgetsMGA.year0?.budget, format: "$#,##0.00" },
+                        { label: "Año 1", value: (row) => row.budgetsMGA.year1?.budget, format: "$#,##0.00" },
+                        { label: "Año 2", value: (row) => row.budgetsMGA.year2?.budget, format: "$#,##0.00" },
+                        { label: "Año 3", value: (row) => row.budgetsMGA.year3?.budget, format: "$#,##0.00" },
+                        { label: "Año 4", value: (row) => row.budgetsMGA.year4?.budget, format: "$#,##0.00" },
+                        { label: "Presupuesto", value: (row) => row.productMGA ? row.budgetsMGA.year0?.budget + row.budgetsMGA.year1?.budget + row.budgetsMGA.year2?.budget + row.budgetsMGA.year3?.budget + row.budgetsMGA.year4?.budget : null, format: "$#,##0.00" },
                         { label: "Vigencia", value: (row) => row.validity },
                         { label: "Año", value: (row) => row.year },
-                        { label: "Actividad detallada", value: (row) => `${row.detailActivity.consecutive}. ${row.detailActivity.detailActivity}` },
-                        { label: "Componente", value: (row) => row.detailActivity.component },
-                        { label: "Unidad de medida", value: (row) => row.detailActivity.measurement },
-                        { label: "Cantidad", value: (row) => row.detailActivity.amount },
-                        { label: "Costo unitario", value: (row) => row.detailActivity.unitCost, format: "$0.00" },
-                        { label: "Costo total", value: (row) => row.detailActivity.unitCost * row.detailActivity.amount, format: "$0.00" },
-                        { label: "Objeto de gasto POSPRE", value: (row) => row.detailActivity.pospre },
-                        { label: "Validador CPC", value: (row) => row.detailActivity.validatorCPC },
-                        { label: "Clasificador CPC", value: (row) => row.detailActivity.clasificatorCPC },
-                        { label: "Validador sección CPC", value: (row) => row.detailActivity.sectionValidatorCPC },
+                        { label: "Actividad detallada", value: (row) => row.detailActivity?.consecutive ? `${row.detailActivity.consecutive}. ${row.detailActivity.detailActivity}` : null },
+                        { label: "Componente", value: (row) => row.detailActivity?.component },
+                        { label: "Unidad de medida", value: (row) => row.detailActivity?.measurement },
+                        { label: "Cantidad", value: (row) => row.detailActivity?.amount },
+                        { label: "Costo unitario", value: (row) => row.detailActivity?.unitCost, format: "$#,##0.00" },
+                        { label: "Costo total", value: (row) => row.detailActivity?.unitCost ? row.detailActivity.unitCost * row.detailActivity.amount : null, format: "$#,##0.00" },
+                        { label: "Objeto de gasto POSPRE", value: (row) => row.detailActivity?.pospre },
+                        { label: "Validador CPC", value: (row) => row.detailActivity?.validatorCPC },
+                        { label: "Clasificador CPC", value: (row) => row.detailActivity?.clasificatorCPC },
+                        { label: "Validador sección CPC", value: (row) => row.detailActivity?.sectionValidatorCPC },
                     ],
                     content: content,
                 }
