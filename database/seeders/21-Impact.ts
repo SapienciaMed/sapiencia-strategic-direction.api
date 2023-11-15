39c4f630067083da8016be0c0bfe367ad9342732
davidhh98 <136001294+dhenao-i4digital@users.noreply.github.com>
2023-11-14 19:20:48 -0500

    ajuste seeders

---
+++ database/seeders/21-Impact.ts
@@ -0,0 +1,40 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import impacto from "App/Models/Impact";
+
+export default class extends BaseSeeder {
+  public async run () {
+    // Write your database queries inside the run method
+    await impacto.createMany([
+      {
+        id: 1,
+        description: 'Insignificante',
+        active: true,
+        order: 1 
+      },
+      {
+        id: 2,
+        description: 'Menor',
+        active: true,
+        order: 2
+      },
+      {
+        id: 3,
+        description: 'Moderado',
+        active: true,
+        order: 3
+      },
+      {
+        id: 4,
+        description: 'Mayor',
+        active: true,
+        order: 4
+      },
+      {
+        id: 5,
+        description: 'Catastrófico',
+        active: true,
+        order: 5
+      }
+    ]);
+  }
+}
